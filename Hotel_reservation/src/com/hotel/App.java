af849ead41e0998c26635e3313977f98aa65a578
Do-it-chu <103571758+Do-it-chu@users.noreply.github.com>
2022-05-16 10:36:05 +0900

    Add files via upload

---
+++ Hotel_reservation/src/com/hotel/App.java
@@ -0,0 +1,10 @@
+package com.hotel;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("-+-+-+-+-호텔 예약 시스템-+-+-+-+-");
+		System.out.println("‡   1. 고객 명단 확인                        ‡");
+	}
+
+}
